8576cace855c72eb784f91f47010e6db89be4a7a
gruchh <kubol2@gmail.com>
2025-10-09 20:26:54 +0200

    Run config server, eureka & employee-service

--- services/employee-service/src/main/java/com/automationcompany/employee/EmployeeServiceApplication.java
+++ services/employee-service/src/main/java/com/automationcompany/employee/EmployeeServiceApplication.java
@@ -1,7 +1,8 @@
-package com.automationcompany.employee.employee_service;
+package com.automationcompany.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class EmployeeServiceApplication {

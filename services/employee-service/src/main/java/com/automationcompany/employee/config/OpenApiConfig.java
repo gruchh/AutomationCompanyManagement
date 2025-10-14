c2729404f22c017279360386b26e4d68d5fe524e
gruchh <kubol2@gmail.com>
2025-10-14 18:22:31 +0200

    Properties to .yml conversion + swagger enable

---
+++ services/employee-service/src/main/java/com/automationcompany/employee/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.automationcompany.employee.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("/employee-service").description("Access via API Gateway")
+                ));
+    }
+}

e933efcc8fc694600228a76a0f82d1cf3114d656
gruchh <kubol2@gmail.com>
2026-04-15 17:52:00 +0200

    Use new controler version with api:generate:project with new path

---
+++ frontend/src/app/features/dashboard/projects/service/generated/api/api.ts
@@ -0,0 +1,3 @@
+export * from './projectController.service';
+import { ProjectControllerApi } from './projectController.service';
+export const APIS = [ProjectControllerApi];
---
+++ frontend/src/app/features/dashboard/projects/service/generated/model/models.ts
@@ -0,0 +1,2 @@
+export * from './projectCardDto';
+export * from './projectFilterDto';
